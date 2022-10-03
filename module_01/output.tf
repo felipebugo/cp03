ae3da7b34ce99892189a0aa013e06cc5ce116883
Felipe Rafael Franco Meira <45401843+felipebugo@users.noreply.github.com>
2022-10-03 17:34:44 -0300

    output.tf

---
+++ module_01/output.tf
@@ -0,0 +1,22 @@
+# VPC
+output "vpc_id" {
+  value = aws_vpc.vpc10.id
+}
+
+# PUBLIC SUBNETS
+output "sn_pub_1a_id" {
+  value = aws_subnet.sn_pub_1a.id
+}
+
+output "sn_pub_1c_id" {
+  value = aws_subnet.sn_pub_1c.id
+}
+
+# PRIVATE SUBNETS
+output "sn_priv_1a_id" {
+  value = aws_subnet.sn_priv_1a.id
+}
+
+output "sn_priv_1c_id" {
+  value = aws_subnet.sn_priv_1c.id
+}
